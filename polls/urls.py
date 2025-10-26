14f9ef54df15105ade3121c1d38f7d0340c35af8
Armando Urquiola <patchamama.go@gmail.com>
2025-10-26 19:16:56 +0100

    feat: Implement user registration

--- polls/urls.py
+++ polls/urls.py
@@ -5,5 +5,6 @@ from . import views
 urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='polls/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('register/', views.SignUpView.as_view(), name='register'),
     path('', views.home, name='home'),
 ]
