3a667734a4589e0a8d62fab7d1285388949bc947
Armando Urquiola <patchamama.go@gmail.com>
2025-10-26 19:08:31 +0100

    feat: Restrict voting to one vote per user

--- polls/models.py
+++ polls/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Candidate(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class Vote(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    candidate = models.ForeignKey(Candidate, on_delete=models.CASCADE)
+    voted_at = models.DateTimeField(auto_now_add=True)
--- polls/views.py
+++ polls/views.py
@@ -1,6 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
+from .models import Candidate, Vote
+from django.db import IntegrityError
 
 @login_required(redirect_field_name=None)
 def home(request):
-    return render(request, 'polls/home.html')
+    candidates = Candidate.objects.all()
+    user_vote = None
+    if request.user.is_authenticated:
+        try:
+            user_vote = Vote.objects.get(user=request.user)
+        except Vote.DoesNotExist:
+            user_vote = None
+
+    if request.method == 'POST' and not user_vote:
+        candidate_id = request.POST.get('candidate')
+        if candidate_id:
+            candidate = Candidate.objects.get(pk=candidate_id)
+            # Since user is a OneToOneField, creating a new vote will fail if one already exists.
+            # We can use update_or_create to handle this case.
+            Vote.objects.update_or_create(user=request.user, defaults={'candidate': candidate})
+        return redirect('home')
+    
+    votes = Vote.objects.all()
+    return render(request, 'polls/home.html', {'candidates': candidates, 'votes': votes, 'user_vote': user_vote})
